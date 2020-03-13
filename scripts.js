66b5dfa45dc5caf59d55f726db064b40e96a9a4e
Kyler Boone <kylerboone18@gmail.com>
2020-03-13 16:32:49 -0500

    add scripts

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
+window.onload = (event) => {
+    alert('page is fully loaded');
+  };
+  
+  
+// document.getElementById("takeoff").addEventListener("click", confirm());
+  
+function confirm(){
+    alert('Confirm that the shuttle is ready for takeoff.');
+}
+//   function confirm(){
+//       window.alert("Confirm that the shuttle is ready for takeoff.");
+//   }
+//   document.getElementById("on-click").addEventListener("click", function (event) {
+//       console.log("toolbar clicked");
+//   });
+//   document.addEventListener("on-click", function (event) {
+//       console.log("document clicked");
+//   });
+  
 // Remember to pay attention to page loading!
