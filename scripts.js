2e29aaaacfdc556c16c9f4ea308445b4e4a9bf30
Your Name <you@example.com>
2020-03-15 15:30:28 -0500

    added javascript changes

--- scripts.js
+++ scripts.js
@@ -1,22 +1,46 @@
 // Write your JavaScript code here.
-window.onload = (event) => {
-    alert('page is fully loaded');
-  };
-  
-  
-// document.getElementById("takeoff").addEventListener("click", confirm());
-  
-function confirm(){
-    alert('Confirm that the shuttle is ready for takeoff.');
+// window.onload = (event) => {
+//     alert('page is fully loaded');
+//   };
+
+function takeoffConfirm() {
+  let x = confirm('Confirm that the shuttle is ready for takeoff.');
+  if(x == true){
+  document.getElementById("flightStatus").innerHTML='Shuttle in flight.';
+  document.getElementById("shuttleBackground").style.backgroundColor = 'blue';
+  document.getElementById("spaceShuttleHeight").innerHTML='10,000';
+  }
 }
-//   function confirm(){
-//       window.alert("Confirm that the shuttle is ready for takeoff.");
-//   }
-//   document.getElementById("on-click").addEventListener("click", function (event) {
-//       console.log("toolbar clicked");
-//   });
-//   document.addEventListener("on-click", function (event) {
-//       console.log("document clicked");
-//   });
-  
+
+function landConfirm() {
+  alert("The shuttle is landing. Landing gear engaged.");
+  document.getElementById("flightStatus").innerHTML='The Shuttle has landed.';
+  document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+  document.getElementById("spaceShuttleHeight").innerHTML='0';
+}
+
+function abortMission() {
+  let x = confirm('Confirm that you want to abort the mission.');
+  if(x == true){
+  document.getElementById("flightStatus").innerHTML='Mission aborted.';
+  document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+  document.getElementById("spaceShuttleHeight").innerHTML='0';
+  }
+}
+
+function moveRocketUp() {
+  // document.getElementById("rocket").style.position = 'absolute';
+  document.getElementById("rocket").style.marginTop += '10px';
+  // document.getElementById("rocket").style.left = '10px';
+}
+let upButton = document.getElementById('upButton');
+let abort = document.getElementById('missionAbort');
+let takeoff = document.getElementById("takeoff");
+let landing = document.getElementById("landing");
+
+upButton.addEventListener("click", moveRocketUp);
+takeoff.addEventListener("click", takeoffConfirm);
+landing.addEventListener("click", landConfirm);
+abort.addEventListener("click", abortMission);
+
 // Remember to pay attention to page loading!
