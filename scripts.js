f903d28ab962492eaeb73f6d9ce2f3a9c4bfe8f2
Kyler Boone <kylerboone18@gmail.com>
2020-03-15 15:52:14 -0500

    fixed directional buttons

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // Write your JavaScript code here.
 // window.onload = (event) => {
-//     alert('page is fully loaded');
+//     alert('Page is fully loaded');
 //   };
 
 function takeoffConfirm() {
-  let x = confirm('Confirm that the shuttle is ready for takeoff.');
+  const x = confirm('Confirm that the shuttle is ready for takeoff.');
   if(x == true){
   document.getElementById("flightStatus").innerHTML='Shuttle in flight.';
   document.getElementById("shuttleBackground").style.backgroundColor = 'blue';
@@ -27,20 +27,38 @@ function abortMission() {
   document.getElementById("spaceShuttleHeight").innerHTML='0';
   }
 }
+//rocket
+const rocket = document.getElementById('rocket');
+//command buttons
+const abort = document.getElementById('missionAbort');
+const takeoff = document.getElementById("takeoff");
+const landing = document.getElementById("landing");
 
-function moveRocketUp() {
-  // document.getElementById("rocket").style.position = 'absolute';
-  document.getElementById("rocket").style.marginTop += '10px';
-  // document.getElementById("rocket").style.left = '10px';
-}
-let upButton = document.getElementById('upButton');
-let abort = document.getElementById('missionAbort');
-let takeoff = document.getElementById("takeoff");
-let landing = document.getElementById("landing");
+//directional buttons
+const upButton = document.getElementById('upButton');
+const downButton = document.getElementById('downButton');
+const leftButton = document.getElementById('leftButton');
+const rightButton = document.getElementById('rightButton');
 
-upButton.addEventListener("click", moveRocketUp);
-takeoff.addEventListener("click", takeoffConfirm);
-landing.addEventListener("click", landConfirm);
-abort.addEventListener("click", abortMission);
+//Event listeners
+takeoff.addEventListener('click', takeoffConfirm);
+landing.addEventListener('click', landConfirm);
+abort.addEventListener('click', abortMission);
 
+leftButton.addEventListener('click', function () {
+    console.log('leftButton', rocket.style.left);
+    rocket.style.left = String(parseInt(rocket.style.left, 10) - 10) + 'px';
+});
+rightButton.addEventListener('click', function () {
+    console.log('rightButton', rocket.style.left);
+    rocket.style.left = String(parseInt(rocket.style.left, 10) + 10) + 'px';
+});
+upButton.addEventListener('click', function () {
+    console.log('upButton', rocket.style.bottom);
+    rocket.style.bottom = String(parseInt(rocket.style.bottom, 10) + 10) + 'px';
+});
+downButton.addEventListener('click', function () {
+    console.log('downButton', rocket.style.bottom);
+    rocket.style.bottom = String(parseInt(rocket.style.bottom, 10) - 10) + 'px';
+});
 // Remember to pay attention to page loading!
